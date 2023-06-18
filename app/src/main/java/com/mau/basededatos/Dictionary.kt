567a25f1da652e8603a2752145c6a4a385ab17cf
MauLap <mfloresgarzofino67@gmail.com>
2023-06-17 21:47:08 -0400

    Boton Dic enlazado

--- app/src/main/java/com/mau/basededatos/Dictionary.kt
+++ app/src/main/java/com/mau/basededatos/Dictionary.kt
@@ -1,51 +1,70 @@
 package com.mau.basededatos
 
-import android.content.Intent
-import android.os.Bundle
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.button.MaterialButton
-import com.google.firebase.firestore.FirebaseFirestore
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SearchView
+import com.google.firebase.database.*
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class Dictionary : AppCompatActivity() {
-    private lateinit var editTextSearch: EditText
-    private lateinit var btnSearch: MaterialButton
-    private lateinit var txtResults: TextView
 
-    private val db = FirebaseFirestore.getInstance()
-    private val dictionaryCollection = db.collection("dictionary")
+    private lateinit var listView: ListView
+    private lateinit var searchView: SearchView
+    private lateinit var adapter: ArrayAdapter<String>
+    private lateinit var dataList: ArrayList<String>
+    private lateinit var databaseRef: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dictionary)
 
-        editTextSearch = findViewById(R.id.editTxtSearch)
-        btnSearch = findViewById(R.id.btnSubmit)
-        txtResults = findViewById(R.id.txtViewResult)
-        btnSearch.setOnClickListener {
-            val searchWord = editTextSearch.text.toString().trim()
-            searchDictionary(searchWord)
-        }
+        listView = findViewById(R.id.list_view)
+        searchView = findViewById(R.id.search_view)
+
+        dataList = ArrayList()
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, dataList)
+        listView.adapter = adapter
+
+        databaseRef = FirebaseDatabase.getInstance().reference.child("dictionary")
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                filterDictionary(newText)
+                return false
+            }
+        })
+
+        retrieveDictionary()
     }
 
-    private fun searchDictionary(word: String) {
-        dictionaryCollection
-            .whereEqualTo("word", word)
-            .get()
-            .addOnSuccessListener { querySnapshot ->
-                if (!querySnapshot.isEmpty) {
-                    val resultDocument = querySnapshot.documents[0]
-                    val typeWord = resultDocument.getString("typeWord")
-                    val definition = resultDocument.getString("description")
-                    val resultText = "$typeWord\n$definition"
-                    txtResults.text = resultText
-                } else {
-                    txtResults.text = "Word not found"
+    private fun retrieveDictionary() {
+        databaseRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                dataList.clear()
+                for (wordSnapshot in dataSnapshot.children) {
+                    val word = wordSnapshot.key.toString()
+                    dataList.add(word)
                 }
+                adapter.notifyDataSetChanged()
             }
-            .addOnFailureListener { exception ->
-                txtResults.text = "Error: ${exception.message}"
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Manejar el error al recuperar los datos del diccionario
             }
+        })
+    }
+
+    private fun filterDictionary(query: String) {
+        val filteredData = dataList.filter { it.contains(query) }
+        adapter.clear()
+        adapter.addAll(filteredData)
+        adapter.notifyDataSetChanged()
     }
 }
