f4e47b9e27a24dceeb3b3851838deb75bac85e02
MauLap <mfloresgarzofino67@gmail.com>
2023-06-18 18:28:34 -0400

    Traduc y Quiz

--- app/src/main/java/com/mau/basededatos/UI.kt
+++ app/src/main/java/com/mau/basededatos/UI.kt
@@ -3,6 +3,8 @@ package com.mau.basededatos
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
 
 class UI : AppCompatActivity() {
@@ -11,6 +13,7 @@ class UI : AppCompatActivity() {
     private lateinit var bVocabulario: LinearLayout
     private lateinit var bHist: LinearLayout
     private lateinit var bJuegos: LinearLayout
+    private lateinit var bsalirCuenta: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ui)
@@ -18,22 +21,28 @@ class UI : AppCompatActivity() {
         bVocabulario = findViewById(R.id.vocabulary)
         bHist = findViewById(R.id.record)
         bJuegos = findViewById(R.id.games)
+        bsalirCuenta = findViewById(R.id.logout)
 
-        bTraductor.setOnClickListener {
+        bTraductor.setOnClickListener {view ->
             val intent = Intent(this@UI, VistaTraductor::class.java)
             startActivity(intent)
         }
-        bVocabulario.setOnClickListener {
+        bVocabulario.setOnClickListener {view ->
             val intent = Intent(this@UI, MainActivity::class.java)
             startActivity(intent)
         }
-        bHist.setOnClickListener {
+        bHist.setOnClickListener {view ->
             val intent = Intent(this@UI, MainActivity::class.java)
             startActivity(intent)
         }
-        bJuegos.setOnClickListener {
+        bJuegos.setOnClickListener {view ->
             val intent = Intent(this@UI, Quiz::class.java)
             startActivity(intent)
         }
+        bsalirCuenta.setOnClickListener {view ->
+            val intent = Intent(this@UI, Login::class.java)
+            startActivity(intent)
+        }
+
     }
 }
